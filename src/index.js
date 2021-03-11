abed7d8eddab17375e2acbb36308953e302bfcf3
Olga <smirnova_olga18@mail.ru>
2021-03-11 10:29:19 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsArr = bracketsConfig.map(item => item.join(""));
+  for (let i = 0; i < bracketsArr.length; i++) {
+    if (str.includes(bracketsArr[i])) {
+      str = str.replace(bracketsArr[i], "");
+      i = -1;
+    }
+  }
+  return !str;
+};
+
+
